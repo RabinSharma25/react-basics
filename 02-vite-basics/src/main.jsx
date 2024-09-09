ccbc38dc122ea402be30a2771f996ca96704c19a
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-09-09 16:13:03 +0530

    Learned how react works underneath

--- 02-vite-basics/src/main.jsx
+++ 02-vite-basics/src/main.jsx
@@ -1,12 +1,22 @@
 import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import Rabin from './Rabin.jsx'
 
+//Lets design our own react element using objects. Reacts actually sees all the written html  as objects and not as actual html we write
 
+const reactElement = React.createElement(
+    'a', // Type of element (e.g., div, span, h1)
+    {href: 'https://google.com', target:'_blank'}, // Props (attributes) for the element
+    'click to visit googl' // Children (content inside the element)
+
+)
 createRoot(document.getElementById('root')).render(
-    <>
-    <App/>
-    <Rabin/>
-    </>
+    // <>
+    // <App/>
+    // <Rabin/>
+    // </>
+    reactElement
+    
 )
