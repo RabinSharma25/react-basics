38d5ed20494c9f52234a8e1fd492a9c157b747b2
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-09-10 20:23:21 +0530

    made a password generator and learned about hooks likuseState, useEffect, useRef.

--- 07-password-generator/src/App.jsx
+++ 07-password-generator/src/App.jsx
@@ -1,35 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState(" ");
 
+  const passwordRef = useRef(null);
+
+  const generatePassword = useCallback(() => {
+    let pass = "";
+    let alphabets = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let numbers = "0123456789";
+    let specialChars = "!@#$%^&*()-_=+[]{}|;:',.<>?/`~";
+    let str = alphabets;
+    if (charAllowed) str += specialChars;
+    if (numberAllowed) str += numbers;
+    for (let i = 1; i < length; i++) {
+      pass += str.charAt(Math.floor(Math.random() * str.length + 1));
+    }
+
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
+
+  useEffect(() => {
+    generatePassword();
+  }, [length, charAllowed, numberAllowed]);
+
+  const copyPasswordToClipboard = () => {
+    window.navigator.clipboard.writeText(password);
+    // passwordRef.current.select()
+    passwordRef.current?.select(); // safer method
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 ">
+      <h1 className="text-white text-center my-3">Password Generator</h1>
+      <div className=" bg-red-500 flex shodow rounded-lg overflow-hidden mb-4">
+        <input
+          type="text"
+          value={password}
+          className="outline-none w-full py-1 px-3"
+          placeholder="password"
+          readOnly
+          ref={passwordRef}
+        />
+        <button
+          className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+          onClick={copyPasswordToClipboard}
+        >
+          Copy
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <div className="flex text-sm gap-x-2">
+        <div className="flex items-center gap-x-1">
+          <input
+            type="range"
+            min={8}
+            max={18}
+            value={length}
+            name=""
+            id=""
+            className="cursor-pointer"
+            onChange={(e) => setLength(e.target.value)}
+          />
+          <label htmlFor="length">length {length}</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            className="cursor-pointer"
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="numbers">Numbers</label>
+        </div>
+        <div className="flex items-center gap-x-1">
+          <input
+            type="checkbox"
+            defaultChecked={charAllowed}
+            name=""
+            id=""
+            className="cursor-pointer"
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
+          <label htmlFor="characterInput">Characters</label>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
