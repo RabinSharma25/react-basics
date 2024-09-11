66f69c2cfc7889ab79c1a394b4c0c1bdaa46ba73
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-09-11 21:51:52 +0530

    Learned about routing in react usng the react-rouact-router-dom  lib

---
+++ 09-react-router/src/components/github/Github.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+//   const [data, setData] = React.useState([]);
+
+//   useEffect(() => {
+//     fetch("https://api.github.com/users/RabinSharma25")
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data);
+//         setData(data);
+//       });
+//   }, []);
+const data = useLoaderData()
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img className="rounded-full  w-52" src={data.avatar_url} alt="_blank" />
+    </div>
+  );
+}
+
+
+
+ const githubInfoLoader = async ()=>{
+    const res = await fetch("https://api.github.com/users/RabinSharma25")
+    return res.json()
+}
+
+export {Github,githubInfoLoader};
