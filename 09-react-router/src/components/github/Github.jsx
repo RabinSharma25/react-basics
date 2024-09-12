217d4d9109dea2ed23cdf9cdebd22be88b4ee87f
RabinSharma25 <cruzosharmas22545@gmail.com>
2024-09-12 13:08:44 +0530

    formatted the code

--- 09-react-router/src/components/github/Github.jsx
+++ 09-react-router/src/components/github/Github.jsx
@@ -3,17 +3,17 @@ import { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function Github() {
-//   const [data, setData] = React.useState([]);
+  //   const [data, setData] = React.useState([]);
 
-//   useEffect(() => {
-//     fetch("https://api.github.com/users/RabinSharma25")
-//       .then((res) => res.json())
-//       .then((data) => {
-//         console.log(data);
-//         setData(data);
-//       });
-//   }, []);
-const data = useLoaderData()
+  //   useEffect(() => {
+  //     fetch("https://api.github.com/users/RabinSharma25")
+  //       .then((res) => res.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
+  const data = useLoaderData();
   return (
     <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
       Github followers: {data.followers}
@@ -22,11 +22,9 @@ const data = useLoaderData()
   );
 }
 
+const githubInfoLoader = async () => {
+  const res = await fetch("https://api.github.com/users/RabinSharma25");
+  return res.json();
+};
 
-
- const githubInfoLoader = async ()=>{
-    const res = await fetch("https://api.github.com/users/RabinSharma25")
-    return res.json()
-}
-
-export {Github,githubInfoLoader};
+export { Github, githubInfoLoader };
